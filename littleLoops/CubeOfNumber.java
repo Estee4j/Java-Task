9511eb7b2add7610700ca4b52e8e59cb7d9963e0
Estee4j <estherokagwa@gmail.com>
2026-04-09 07:34:06 +0100

    littlelopps

---
+++ littleLoops/CubeOfNumber.java
@@ -0,0 +1,22 @@
+public class  CubeOfNumber {
+
+ public static void main(String [] args) { 
+
+     System.out.println("cubes of number 1 to 5");
+
+     for (int i = 1; i <= 5; i++) {
+      System.out.println(i * i * i);
+
+
+
+}
+
+
+
+
+
+
+}
+
+
+}
