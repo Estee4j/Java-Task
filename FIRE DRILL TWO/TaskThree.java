75533507cef587dbe2709bcef13af120212d9568
Estee4j <estherokagwa@gmail.com>
2026-04-15 17:07:28 +0100

    Added a new folder

---
+++ FIRE DRILL TWO/TaskThree.java
@@ -0,0 +1,109 @@
+import java.util.Scanner;
+    
+  public class TaskThree {
+  
+    public static void main(String [] args) {
+
+     Scanner input = new Scanner(System.in);
+
+
+   System.out.println("Enter first  number: ");
+    int number1 = input.nextInt();
+
+
+ System.out.println("Enter second number: ");
+    int number2 = input.nextInt();
+
+  
+ System.out.println("Enter third  number: ");
+    int number3 = input.nextInt();
+
+
+
+System.out.println("Enter fourth  number: ");
+    int number4 = input.nextInt();
+
+
+
+System.out.println("Enter fifth  number: ");
+    int number5 = input.nextInt();
+
+
+
+System.out.println("Enter sixth number: ");
+    int number6 = input.nextInt();
+
+
+
+System.out.println("Enter seventh number: ");
+    int number7 = input.nextInt();
+
+
+
+System.out.println("Enter eight number: ");
+    int number8 = input.nextInt();
+
+
+
+System.out.println("Enter nineth number: ");
+    int number9 = input.nextInt();
+
+
+
+System.out.println("Enter tenth  number: ");
+    int number10 = input.nextInt();
+
+
+
+
+
+  int sum = number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10;
+
+ int average = number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10 / 10;
+
+  System.out.println("The sum is " + sum);
+ System.out.println("The average number is " + average);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+}
+
+
+
+
