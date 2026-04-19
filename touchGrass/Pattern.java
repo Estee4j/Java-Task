f67b8883582fadcb940bd86efcc49aec2ed4db27
Estee4j <estherokagwa@gmail.com>
2026-04-20 00:31:31 +0100

    touchGrass

---
+++ touchGrass/Pattern.java
@@ -0,0 +1,16 @@
+public class Pattern {
+    
+ public static void main(String[] args){
+
+  for (int count = 7; count >= 1; count--) {
+    System.out.print(count + " ");
+
+}
+       for (int count = 2; count <= 7; count++) {
+    System.out.print(count + " ");
+ }   
+
+System.out.println();
+
+}
+}
