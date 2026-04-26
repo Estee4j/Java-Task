902745a34f0bb9131e4410311ebef9e5020f3324
Estee4j <estherokagwa@gmail.com>
2026-04-26 15:40:36 +0100

    Wahala

---
+++ wahala/PizzaWahala.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+ public class Pizzawahala {
+
+  public static void main(String [] args){
+
+   Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter the number of guest: ");
+     int guest = input.nextInt();
+      input.nextLine();
+
+      System.out.println("Enter the pizza type: ");
+        String type = input.nextLine();
+
+        
+         int price = 0;
+         int slice = 0;
+
+       System.out.print("""
+          
+                       1. Sapa size
+                       2. Small Money
+                       3. Big Boys
+                       4. Odogwu
+
+
+                     """);
+
+  
+          System.out.print("Enter options: ");
+           int options = input.nextInt();
+            switch option {
+
+                    case 1 ->{ System.out.println("Sapa Size: ")
+                                        price = 2500;
+                                        slice = 4;
+
+}
+
+                    case 2 ->{ 
+
+}
+}
